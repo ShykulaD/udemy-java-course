3218388891dc3925aef48b8bcb06c7b20c887974
ShykulaD <de.shykula@gmail.com>
2020-12-29 15:05:13 +0200

    test

--- src/test/java/Program.java
+++ src/test/java/Program.java
@@ -4,8 +4,11 @@
 public class Program {
 
     public static void main(String[] args) {
-        Animal dog = new Dog();
-        dog.animalInt = 4;
-        System.out.println(dog.animalInt);
+        Dog dog = new Dog();
+        Animal dog2 = new Dog();
+
+
+
     }
+
 }
